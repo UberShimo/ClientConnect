67478ce7c6d5ba2045ee21797e1f57fdbd40ef4b
Simon Seabird <m13simon@gmail.com>
2019-03-23 15:44:28 +0000

    gutt

--- src/main/java/iths/helena/ClientConnect.java
+++ src/main/java/iths/helena/ClientConnect.java
@@ -3,18 +3,16 @@ package iths.helena;
 import org.springframework.web.client.RestTemplate;
 
 public class ClientConnect {
-
     public static void main(String[] args) {
-        getEmployees();
+        getSongs();
     }
 
-    private static void getEmployees(){
-        final String uri = "http://restlabb-env.rntvyhfjkd.eu-north-1.elasticbeanstalk.com/employees/";
+    private static void getSongs(){
+        final String url = "https://simon-herokuserver.herokuapp.com/songs";
 
         RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
+        String result = restTemplate.getForObject(url, String.class);
 
         System.out.println(result);
     }
-
 }
